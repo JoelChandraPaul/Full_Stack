7920f409db6fbcaaa246fc08dfae73cda5e46da5
Joel Chandra Paul <joelcpaul02@gmail.com>
2026-01-20 17:33:26 -0500

    Task Completed

---
+++ Day_2(Methods)/NumberAnalyzer/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace NumberAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunProgram();
+        }
+
+        static void RunProgram()
+        {
+            while (true)
+            {
+                string input = GetInput();
+
+                if (ShouldExit(input))
+                {
+                    Console.WriteLine("Exiting the program. Goodbye!...");
+                    break;
+                }
+
+                if (!TryGetNumber(input, out int num))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                    continue;
+                }
+
+                EvenOrOdd(num);
+                PositiveOrNegative(num);
+
+                Console.WriteLine(); 
+            }
+        }
+
+        static string GetInput()
+        {
+            Console.WriteLine("Enter a number:\n Type q or exit to Stop the program");
+            return Console.ReadLine().Trim().ToLower();
+        }
+
+        static bool ShouldExit(string input)
+        {
+            if (input == "q" || input == "exit")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static bool TryGetNumber(string input, out int number)
+        {
+            if (int.TryParse(input, out number))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void EvenOrOdd(int num)
+        {
+            if(num%2 == 0)
+            {
+                Console.WriteLine($"{num} is Even");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is Odd");
+            }
+        }
+
+        static void PositiveOrNegative(int num)
+        {
+            if(num > 0)
+            {
+                Console.WriteLine($"{num} is Positive");
+            }
+            else if(num == 0)
+            {
+                Console.WriteLine($"{num} is neither Positive nor Negative");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is Negative");
+            }
+        }
+    }
+}
