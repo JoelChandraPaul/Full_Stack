c474bae11dfd3e4974f8cadba11d2ac99a32d618
Joel Chandra Paul <joelcpaul02@gmail.com>
2026-01-20 16:48:02 -0500

    Add project files.

---
+++ Day_1(Basics)/PositiveNegativeApp/Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace PositiveNegativeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a number (or press q to quit):");
+
+                string input = Console.ReadLine().Trim().ToLower();
+
+                if (input == "q")
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+
+                bool isValid = int.TryParse(input, out int num);
+
+                if (!isValid)
+                {
+                    Console.WriteLine("Please enter a valid number.\n");
+                    continue;
+                }
+
+                if (num == 0)
+                {
+                    Console.WriteLine("The number is neither positive nor negative\n");
+                }
+                else if (num > 0)
+                {
+                    Console.WriteLine("The number is positive\n");
+                }
+                else
+                {
+                    Console.WriteLine("The number is negative\n");
+                }
+            }
+        }
+    }
+}
+f
